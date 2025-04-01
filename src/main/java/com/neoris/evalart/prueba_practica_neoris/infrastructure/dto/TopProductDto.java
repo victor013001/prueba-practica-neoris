7d2b685f8f63283c7052be1b37d87b02ecfbf2cf
Victor Manuel Osorio Garcia <81490294+victor013001@users.noreply.github.com>
2025-04-01 14:42:50 -0500

    feat: get max Stock per branch (#6)

---
+++ src/main/java/com/neoris/evalart/prueba_practica_neoris/infrastructure/dto/TopProductDto.java
@@ -0,0 +1,7 @@
+package com.neoris.evalart.prueba_practica_neoris.infrastructure.dto;
+
+public record TopProductDto(
+        String branchName,
+        String productName,
+        Integer stock
+) {}
